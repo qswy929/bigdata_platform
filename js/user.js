6ef6ddf7ea4c5b6332ef79633a1db21fb7b01507
Ethan Koo <qswy929@sina.com>
2017-03-24 16:25:05 +0800

    增加登录页面的链接

---
+++ js/user.js
@@ -0,0 +1,25 @@
+new Vue ({
+        el: '#app',
+        data : {
+          ope_info: '登录',
+          url: '#'
+        },
+        methods: {
+          urlRoute: function(){
+            if(typeof($.cookie('username'))!="undefined" && $.cookie('username') != null)
+            {
+              this.ope_info = '注销';
+              this.url="member/logout.html";
+            }
+            else
+            {
+              this.ope_info = '登录';
+              this.url="member/login.html";
+            }
+          }
+        },
+        mounted: function() {
+          //alert($.cookie('username'))
+          this.urlRoute();
+        }
+      });
