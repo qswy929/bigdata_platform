255df78fd548edf644f2191162f2fd103eed7a90
Ethan Koo <qswy929@sina.com>
2017-05-03 15:13:54 +0800

    更新任务

--- js/data/task.js
+++ js/data/task.js
@@ -1,11 +1,12 @@
 var task =[
             
             {date:"2017-05-01",title:"互动交流 09期",link:"http://dwz.cn/5Sbfka"},
+            {date:"2017-04-26",title:"R实践第2次",link:"http://dwz.cn/5SXxjV"},
             {date:"2017-04-23",title:"互动交流 08期",link:"http://dwz.cn/5SbfOQ"},
-            {date:"2017-04-15",title:"互动交流 05-06期",link:"http://dwz.cn/5SbiAN"},
-            {date:"2017-04-08",title:"这是任务",link:"#"},
-            {date:"2017-03-25",title:"这是任务",link:"#"},
-            {date:"2017-03-23",title:"这是任务",link:"#"},
+            {date:"2017-04-15",title:"数据挖掘实践第1次",link:"http://dwz.cn/5SXzNE"},
+            {date:"2017-04-12",title:"互动交流 05-06期",link:"http://dwz.cn/5SbiAN"},  
+            {date:"2017-03-25",title:"SQL实践第1次",link:"http://dwz.cn/5SXCdm"},
+            {date:"2017-03-22",title:"R实践第1次",link:"http://dwz.cn/5SXE1L"},
             {date:"2017-03-18",title:"这是任务",link:"#"},
             {date:"2017-03-11",title:"这是任务",link:"#"},
             {date:"2017-03-04",title:"这是任务",link:"#"},
