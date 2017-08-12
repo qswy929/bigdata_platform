a2ac5a3c04437e00b9872e465153d925076bfd01
zhangliqing <623009470@qq.com>
2017-08-12 18:45:12 +0800

    delete extra task

--- js/data/task.js
+++ js/data/task.js
@@ -3,11 +3,5 @@ var task =[
             {date:"2017-05-01",title:"用k近邻算法分类",link:"experimentalProject/knn.html"},
             {date:"2017-04-23",title:"决策树入门",link:"experimentalProject/decisionTree.html"},
             {date:"2017-04-15",title:"随机森林",link:"experimentalProject/randomForest.html"},
-            {date:"2017-03-25",title:"这是任务",link:"#"},
-            {date:"2017-03-23",title:"这是任务",link:"#"},
-            {date:"2017-03-18",title:"这是任务",link:"#"},
-            {date:"2017-03-11",title:"这是任务",link:"#"},
-            {date:"2017-03-04",title:"这是任务",link:"#"},
-            {date:"2017-02-28",title:"这是任务",link:"#"},
-            {date:"2017-02-25",title:"这是任务",link:"#"}
+
           ]
